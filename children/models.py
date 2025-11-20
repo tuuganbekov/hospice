d5de2da05963e92d9709d94c684f0af20bcd312d
tuuganbekov <tuuganbekovb@gmail.com>
2025-11-20 13:36:46 +0600

    feat: phone, diagnosis fields are not required

--- children/models.py
+++ children/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Children(models.Model):
     full_name = models.CharField(verbose_name="ФИО", max_length=255)
-    diagnosis = models.CharField(verbose_name="Диагноз",max_length=255)
+    diagnosis = models.CharField(verbose_name="Диагноз",max_length=255, null=True, blank=True)
     dream_text = models.TextField(verbose_name="Мечта (текст)",)
     gift = models.CharField(verbose_name="Подарок",max_length=255)
     gift_price = models.IntegerField(verbose_name="Цена подарка",default=0, null=True, blank=True)
     active = models.BooleanField(verbose_name="Активна",default=True)
     photo = models.ImageField(verbose_name="Фото",upload_to="avatars/")
-    phone_number = models.CharField("Номер телефона", max_length=30)
+    phone_number = models.CharField("Номер телефона", max_length=30, null=True, blank=True)
     address = models.TextField("Адрес", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
